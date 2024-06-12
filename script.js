a6a4ebc9c530674a1c46df912e67846e4fadf6da
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-11 20:47:19 -0700

    update js get score and possible score

--- script.js
+++ script.js
@@ -76,22 +76,87 @@ const LearnerSubmissions = [
   },
 ];
 
+//checkscore
+//If an assignment is not yet due, do not include it in the results or the average.
+//Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+
 function getLearnerData(course, ag, submissions) {
   // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0, // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833, // late: (140 - 15) / 150
-    },
-  ];
+  let result = [];
+
+  //get unique learners as id key value
+  let uniqueLearnerID = [];
+  for (let i = 0; i < submissions.length; i++) {
+    // Check if the picked element is already printed
+    let j;
+    for (j = 0; j < i; j++)
+      if (submissions[i].learner_id == submissions[j].learner_id) {
+        break;
+      }
+
+    if (i == j) {
+      uniqueLearnerID.push(submissions[i].learner_id);
+    }
+  }
+
+  let score;
+  let posiblescore;
+  //for each student
+  for (let i = 0; i < uniqueLearnerID.length; i++) {
+    let sum = 0;
+    //check submissions for student id
+    for (let j = 0; j < submissions.length; j++) {
+      if (submissions[j].learner_id == uniqueLearnerID[i]) {
+        //console.log(submissions[j].learner_id == uniqueLearnerID[i]); //checkkkkkk
+        //look at assignment id and compare dates
+        for (let k = 0; k < ag.assignments.length; k++) {
+          if (submissions[j].assignment_id == ag.assignments[k].id) {
+            //console.log(submissions[j].assignment_id == ag.assignments[0].id); //checkkkkkk
+            //look at due dates vs submission dates
+            //if not assignment not due yet
+            // if (
+            //   new Date(submissions[j].submission.submitted_at) <
+            //   new Date(ag.assignments[k].due_at)
+            // ) {
+            //   score = 0;
+            //   posiblescore = 0;
+            // }
+            //if late
+            if (
+              new Date(submissions[j].submission.submitted_at) >
+              new Date(ag.assignments[k].due_at)
+            ) {
+              score =
+                submissions[j].submission.score -
+                ag.assignments[k].points_possible * 0.1;
+              posiblescore = ag.assignments[k].points_possible;
+            }
+            //if on time or earlier
+            else {
+              score = submissions[j].submission.score;
+              posiblescore = ag.assignments[k].points_possible;
+            }
+          }
+          console.log(score, posiblescore);
+        }
+      }
+    }
+  }
+
+  // const result = [
+  //   {
+  //     id: 125,
+  //     avg: 0.985, // (47 + 150) / (50 + 150)
+  //     1: 0.94, // 47 / 50
+  //     2: 1.0, // 150 / 150
+  //   },
+  //   {
+  //     id: 132,
+  //     avg: 0.82, // (39 + 125) / (50 + 150)
+  //     1: 0.78, // 39 / 50
+  //     2: 0.833, // late: (140 - 15) / 150
+  //   },
+  // ];
 
   return result;
 }
